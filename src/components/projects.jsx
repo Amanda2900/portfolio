646d2aeddd2e9983512ec791b35152e36e25953b
Amanda DiNoto <amanda.dinoto@gmail.com>
2022-02-02 17:35:31 -0500

    added fourth project and updated resume file

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import './projects.css'
-import SS1 from '../assets/la-screenshot.png';
+import SS4 from '../assets/atf-screenshot.png';
+import SS3 from '../assets/la-screenshot.png';
 import SS2 from '../assets/tl-screenshot.png';
-import SS3 from '../assets/pf-screenshot.png';
+import SS1 from '../assets/pf-screenshot.png';
 
 function Projects() {
   return (
@@ -10,7 +11,17 @@ function Projects() {
       <h1>Projects</h1>
       <div className="project-div">
         <div className="project-img">
-          <img src={SS1} alt="larkspur's apothecary landing page" />
+          <img src={SS4} alt="all the feels landing page" />
+        </div>
+        <div className="project-description">
+          <h2>All The Feels</h2>
+          <p>An application that allows users to upload their own artwork, and comment on and view artwork created by other users. It is a full stack application, made with React, Node.js, Express, MongoDB,and CSS. </p>
+          <p>Project is deployed <a href="https://allofthefeels.herokuapp.com/">here</a>. Github for All The Feels can be viewed <a href="https://github.com/Amanda2900/allTheFeels">here</a>.</p>
+        </div>
+      </div>
+      <div className="project-div">
+        <div className="project-img">
+          <img src={SS3} alt="larkspur's apothecary landing page" />
         </div>
         <div className="project-description">
           <h2>Larkspur's Apothecary</h2>
@@ -24,13 +35,13 @@ function Projects() {
         </div>
         <div className="project-description">
           <h2>This Land</h2>
-          <p>A creature creator where a user can build creatures, view ones made by other users and leave comments on them. It is a full stack application, made with Node, Express, Mongoose, MongoDB, EJS, and CSS.</p>
+          <p>A creature creator where a user can build creatures, view ones made by other users and leave comments on them. It is a full stack application, made with Node.js, Express, Mongoose, MongoDB, EJS, and CSS.</p>
           <p>Project is deployed <a href="https://this-land.herokuapp.com/">here</a>. Github for This Land can be viewed <a href="https://github.com/Amanda2900/this-land">here</a>.</p>
         </div>
       </div>
       <div className="project-div">
         <div className="project-img">
-          <img src={SS3} alt="power four game page" />
+          <img src={SS1} alt="power four game page" />
         </div>
         <div className="project-description">
           <h2>Power Four</h2>
