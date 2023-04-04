487caa96a68e7d1fbfe987f2f0471a7b88ff5451
Amanda DiNoto <amanda.dinoto@gmail.com>
2023-04-04 19:52:14 -0400

    edit bio.jsx file

--- src/components/bio.jsx
+++ src/components/bio.jsx
@@ -14,7 +14,7 @@ function Bio() {
         </div>
         <div className="bio-skills">
           <p>
-            As a professional software engineer, I am dedicated to creating engaging and user-friendly programs that enhance the lives of users. I leverage my resourcefulness and problem-solving skills to tackle complex technical challenges. I am deeply passionate about contributing to a well-organized and open-minded environment that encourages creativity and innovation. Drawing from my background in film and theater production, I possess the ability to adapt quickly to new situations, think critically, and collaborate effectively with team members. My diverse experiences have honed my skills in efficient problem-solving and teamwork, making me a valuable asset to any software engineering team.
+            As a professional software engineer, I am committed to crafting engaging and user-friendly software solutions. I bring resourcefulness and adept problem-solving skills to tackle complex technical challenges. Passionate about fostering a well-organized and open-minded work environment, I adapt quickly to new situations and collaborate effectively with team members. With diverse experiences in film and theater production, I possess efficient problem-solving and teamwork skills, making me a valuable asset to any software engineering team.
           </p>
           <p>
             <span>Skills: </span>JavaScript, HTML, CSS, React, Node.js, Express, Python, Django, Photoshop, SQL, PostgreSQL, MongoDB, Tailwind CSS, Git, GitHub, Agile Workflow, WordPress, and REST APIs.
